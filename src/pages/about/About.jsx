987d991b0063c8070ddb32d5ed7d22d888f0f07f
Arthur Smart <89918737+Arthur-Smart@users.noreply.github.com>
2022-11-27 11:14:51 +0300

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,6 +3,7 @@ import AboutHero from '../../components/abouthero/AboutHero'
 import JuniorTeam from '../../components/juniourteam/JuniorTeam'
 import MissionStatements from '../../components/missionstatements/MissionStatements'
 import Team from '../../components/team/Team'
+import './about.css'
 
 function About() {
   return (
