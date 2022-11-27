23ac734972065d28e7f2ab78408cbc2d65e5273b
Arthur Smart <89918737+Arthur-Smart@users.noreply.github.com>
2022-11-27 11:05:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React, { useEffect } from 'react';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import About from './pages/about/About';
@@ -8,15 +7,12 @@ import Achievements from './pages/achievements/Achievements';
 import Cart from './pages/cart/Cart';
 import Home from './pages/home/Home';
 import MainCart from './pages/maincart/MainCart';
-import {BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { useSelector } from 'react-redux';
 
 function App() {
   const {currentUser} = useSelector(state => state.user)
-  const location = useLocation();
-useEffect(() => {
-  window.scrollTo(0,0);
-}, [location]);
+  
   
   return (
      <BrowserRouter>
